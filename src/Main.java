46a3899a6c7a93252b3ab31f1cc6298d118fe8aa
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2026-01-14 16:55:05 +0100

    working on it

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,20 @@ import Entities.Order;
 import Entities.Product;
 
 void main() {
+    ArrayList<Product> products = new ArrayList<>();
+    products.add(new Product("3332", "book", "book", 10.5));
+    products.add(new Product("3332", "book", "book", 10.5));
+    products.add(new Product("3332", "book", "book", 10.5));
+
+    ArrayList<Order> orders = new ArrayList<>();
+    LocalDate orderDate = new LocalDate(2021, 12, 3);
+    LocalDate deliveryDate = new LocalDate(2023, 4,2);
+    Product product = products.getFirst();
+    Customer customer = new Customer();
+    orders.add(new Order("dt3543","pending", orderDate, deliveryDate, product, customer));
 
 
-    List<Product> priceFilter = Arrays.stream().filter(product -> product.price < 100 && product.category = "Book").toList();
-    List<Order> categoryFilter = Arrays.stream().filter(Order.product _> product.category = "Baby").toList();
-    
+    List<Product> priceFilter = products.stream().filter(product -> product.price < 100 && product.category = "Book").toList();
+    List<Order> categoryFilter = products.stream().filter(Order.product _> product.category = "Baby").toList();
+
 }
